c2c2d753dd6967eb994afa654b5451b343dc8405
Drew Banin <drewbanin@gmail.com>
2024-01-13 14:43:41 -0500

    happier with viz...

--- web/dbdb/src/components/visualizer.tsx
+++ web/dbdb/src/components/visualizer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect, useRef } from 'react';
+import React, { useContext, useMemo, useState, useEffect, useRef } from 'react';
 import { useSub } from '../Hooks.js';
 
 import { formatBytes, formatNumber } from '../Helpers.js';
@@ -30,7 +30,7 @@ const createBuffer = (rows) => {
     const numChannels = 1;
 
     // this is in seconds
-    const totalTime = Math.max(...rows.map(r => r.time + r.length));
+    const totalTime = Math.max(...rows.map(r => r.time + (r.length || 1)));
 
     const audioBuffer = audioCtx.createBuffer(
       numChannels,
@@ -77,58 +77,67 @@ const createBuffer = (rows) => {
     const source = audioCtx.createBufferSource();
     source.buffer = audioBuffer;
 
-    const analyser = audioCtx.createAnalyser();
-    analyser.fftSize = 256;
+    source.connect(audioCtx.destination);
 
-    source.connect(analyser);
-    analyser.connect(audioCtx.destination);
-
-    return [source, analyser, audioCtx, totalTime];
+    return [source, audioCtx, totalTime];
 }
 
-function FrequencyDomainViz({ analyser, offset }) {
-  const [ time, setTime ] = useState(0);
-  const [ xVals, setXVals ] = useState([]);
-  const [ yVals, setYVals ] = useState([]);
+function FrequencyDomainViz({ playing, rows, offset }) {
+  if (!playing) {
+      return null
+  }
 
-  useAnimationFrame(deltaTime => {
-      if (!analyser.current) {
-          return
-      }
-
-      const bufferLength = analyser.current.frequencyBinCount;
-      const dataArray = new Uint8Array(bufferLength);
-      analyser.current.getByteFrequencyData(dataArray);
-
-      const domain = SAMPLE_RATE / 2;
-      const stepLength = domain / dataArray.length;
-
-      const x = [];
-      dataArray.forEach((v, i) => {
-          const val = Math.floor(stepLength * i);
-          x.push(val);
-      });
-
-      const y = dataArray;
-
-      const xArray = x;
-      const yArray = Array.from(y || []);
-
-      setXVals(xArray);
-      setYVals(yArray);
-
-      setTime(t => t + deltaTime);
+  // find tones that are playing at t=offset
+  const relevantRows = rows.filter(row => {
+      return row.time <= offset && (row.time + row.length) > offset
   })
 
-  if (!analyser.current || xVals.length === 0 || yVals.length === 0) {
-      return null;
+  const maxFreq = rows.reduce((acc, val) => {
+      if (val.freq > acc) {
+          return val.freq;
+      } else {
+          return acc;
+      }
+  }, 0)
+
+  const xDomain = maxFreq + 50;
+  // want to create 100 steps...
+  const stepSize = Math.floor(maxFreq / 50);
+  const xVals = [];
+  const yVals = [];
+
+  for (let i=0; i < (xDomain / stepSize); i += 1) {
+      const xStart = Math.floor(i * stepSize);
+      const xEnd = Math.ceil(xStart + stepSize);
+
+      const activeNotes = relevantRows.filter(row => {
+          return row.freq >= xStart && row.freq < xEnd;
+      });
+      // todo : account for amplitude?
+      const numActiveNotes = activeNotes.length;
+      const yVal = numActiveNotes;
+
+      xVals.push(xStart);
+      yVals.push(yVal);
+  }
+
+  if (xVals.length == 0 || yVals.length == 0) {
+      xVals.push(0);
+      yVals.push(0);
+  }
+
+  const formattedYVals = {
+    data: yVals,
+    label: 'Amplitude',
+    type: 'bar',
+    color: '#000000',
   }
 
   return (
     <ResponsiveChartContainer
-      series={[{ data: yVals, label: 'v', type: 'bar', color: '#000000' }]}
-      xAxis={[{ scaleType: 'band', data: xVals }]}
-      yAxis={[{ min: 0, max: 255 }]}
+      series={[ formattedYVals ]}
+      xAxis={[{ scaleType: 'band', data: xVals, min: 0, max: xDomain }]}
+      yAxis={[{ min: 0, max: 1 }]}
       margin={{
         left: 35,
         right: 35,
@@ -137,65 +146,46 @@ function FrequencyDomainViz({ analyser, offset }) {
       }}
     >
         <BarPlot />
-        <ChartsTooltip />
+        <ChartsTooltip trigger={"item"} />
         <ChartsXAxis position="bottom" />
         <ChartsYAxis position="left" />
     </ResponsiveChartContainer>
   );
 }
 
-function TimeDomainViz({ analyser, offset }) {
-  const [ time, setTime ] = useState(0);
-  const [ xVals, setXVals ] = useState([]);
-  const [ yVals, setYVals ] = useState([]);
-
-  useAnimationFrame(deltaTime => {
-      if (!analyser.current) {
-          return
-      }
-
-      const bufferLength = analyser.current.frequencyBinCount;
-      const dataArray = new Uint8Array(bufferLength);
-      // state.analyser.getByteFrequencyData(dataArray);
-      analyser.current.getByteTimeDomainData(dataArray);
-
-      const y = [];
-      dataArray.forEach(val => {
-          const amp = (val / 128) - 1;
-          y.push(amp);
-      });
-      const yArray = Array.from(y || []);
-
-      setXVals(xData => {
-          // pick up x-index where we left off?
-          const x = [];
-          const lastT = xData.length == 0 ? 0 : xData[xData.length - 1];
-
-          dataArray.forEach((v, i) => {
-              const val =  i + lastT;
-              x.push(val);
-          });
-
-          return xData.concat(x)
-      });
-
-      setYVals(yData => yData.concat(yArray));
-
-      setTime(t => t + deltaTime);
-  })
-
-  if (!analyser.current || xVals.length === 0 || yVals.length === 0) {
-      return null;
+function TimeDomainViz({ playing, rows, offset }) {
+  if (!playing) {
+      return null
   }
 
-  const maxWindow = 512;
-  const minX = xVals.length > maxWindow ? xVals[xVals.length - 1 - maxWindow] : 0;;
-  const maxX = xVals.length > maxWindow ? xVals[xVals.length - 1] : maxWindow;
+  const relevantRows = rows.filter(row => {
+      return row.time <= offset && (row.time + row.length) > offset
+  })
+
+  const xDomain = 10000;
+  const xVals = [];
+  const yVals = [];
+  for (let i=0; i < xDomain; i++) {
+      xVals.push(i);
+
+      const scaleFactor = SAMPLE_RATE * 10;
+      // const yVal = sin(i / scaleFactor, 440, 1)
+
+      const freqs = relevantRows.map(row => {
+          const func = row.func === 'sin' ? sin : sqr;
+          return func(i / scaleFactor, row.freq, 1)
+      })
+
+      const yTotal = freqs.reduce((acc, val) => acc + val, 0);
+      const yVal = freqs.length === 0 ? 0 : yTotal / freqs.length;
+      yVals.push(yVal);
+  }
+
 
   return (
     <ResponsiveChartContainer
       series={[{ data: yVals, label: 'v', type: 'line', color: '#000000' }]}
-      xAxis={[{ scaleType: 'linear', data: xVals, min: minX, max: maxX }]}
+      xAxis={[{ scaleType: 'linear', data: xVals, min: 0, max: xDomain }]}
       yAxis={[{ min: -1, max: 1 }]}
       margin={{
         left: 35,
@@ -218,41 +208,62 @@ function Visualizer() {
 
     const [ playing, setPlaying ] = useState(null);
     const [ playTime, setPlayTime ] = useState(null);
-    const [ FFT, setFFT ] = useState(null);
+    const [ pausedAt, setPausedAt ] = useState(null);
 
-    const analyser = useRef(null);
-
+    const source = useRef(null);
+    const audioCtx = useRef(null);
     const state = {}
 
     useSub('QUERY_COMPLETE', (queryId) => {
         setPlaying(queryId);
     });
 
+    const mappedRows = rows.map(row => {
+        const mapped = {};
+        dataSchema.forEach((col, i) => {
+            mapped[col] = row[i];
+        })
+        return mapped;
+    })
+
+    const stopSound = () => {
+        source.current.stop();
+
+        setPlayTime(0);
+        setPlaying(false);
+        state.source = null;
+        state.context = null;
+    }
+
+    const playPauseSound = () => {
+        const currentTime = playTime;
+
+        if (pausedAt !== null) {
+            audioCtx.current.resume();
+            setPausedAt(null);
+        } else {
+            audioCtx.current.suspend();
+            setPausedAt(currentTime);
+        }
+    }
+
     useEffect(() => {
-        if (!playing) {
+        if (!playing || mappedRows.length === 0) {
             return
         }
 
-        const mappedRows = rows.map(row => {
-            const mapped = {};
-            dataSchema.forEach((col, i) => {
-                mapped[col] = row[i];
-            })
-            return mapped;
-        })
-
-        console.log("Playing for query:", playing);
-        const [ newSource, newAnalyser, ctx, totalTime ] = createBuffer(mappedRows);
-
-        analyser.current = newAnalyser;
+        console.log("Playing for query:", playing, mappedRows);
+        const [ newSource, ctx, totalTime ] = createBuffer(mappedRows);
 
         state.endTime = totalTime;
         state.source = newSource;
         state.context = ctx;
-        // state.analyser = analyser;
+
+        audioCtx.current = ctx;
+        source.current = newSource;
 
         newSource.start();
-    }, [playing, analyser])
+    }, [playing, source, audioCtx])
 
     useEffect(() => {
       const interval = setInterval(() => {
@@ -262,13 +273,6 @@ function Visualizer() {
 
         setPlayTime(state.context.currentTime);
 
-        // state.analyser.fftSize = 64;
-        // const bufferLength = state.analyser.frequencyBinCount;
-        // const dataArray = new Uint8Array(bufferLength);
-        // state.analyser.getByteFrequencyData(dataArray);
-        // state.analyser.getByteTimeDomainData(dataArray);
-        // setFFT(dataArray)
-
         if (state.context.currentTime > state.endTime) {
           console.log("done playing", state.context.currentTime, state.endTime)
           state.source.stop()
@@ -277,12 +281,12 @@ function Visualizer() {
       }, 10);
 
       return () => { console.log("Cancelling interval"); clearInterval(interval); }
-    }, [playing, setPlaying, setPlayTime, setFFT]);
+    }, [playing, setPlaying, setPlayTime]);
 
     const [ vizType, setVizType ] = useState('time');
 
-    const showTime = vizType === 'time' && !!playing;
-    const showFreq = vizType === 'freq' && !!playing;
+    const showTime = vizType === 'time';
+    const showFreq = vizType === 'freq';
 
     // const samples = rows ? rows.map((val) => val[1]) : [];
     // const startIndex = Math.floor(playTime * 44100);
@@ -290,26 +294,41 @@ function Visualizer() {
     // const samples = rows.slice(startIndex, endIndex).map(val => val[1]);
     // console.log("???", startIndex, endIndex, samples.length)
     
-    // {!!playing && <TimeDomainViz analyser={analyser} offset={playTime} />}
     return (
         <>
             <div className="panelHeader">
                 <div style={{ padding: 5 }}>
                     <div className="helpText">
-                        <button
-                            style={{ margin: 0, marginRight: 5, verticalAlign: 'top' }}
-                            onClick={ e => setVizType('freq') }
-                            className="light title">BAR</button>
-                        <button
-                            style={{ margin: 0, verticalAlign: 'top' }}
-                            onClick={ e => setVizType('time') }
-                            className="light title">LINE</button>
+                            <>
+                                <button
+                                    style={{ margin: 0, marginRight: 5, verticalAlign: 'top' }}
+                                    onClick={ e => setVizType('freq') }
+                                    className="light title">FREQ</button>
+                                <button
+                                    style={{ margin: 0, verticalAlign: 'top' }}
+                                    onClick={ e => setVizType('time') }
+                                    className="light title">TIME</button>
+
+                                <div style={{ margin: 0, float: 'right' }}>
+                                    <button
+                                        style={{ margin: 0, marginRight: 5, verticalAlign: 'top' }}
+                                        onClick={ e => playPauseSound() }
+                                        className="light title">
+                                        { pausedAt === null ? 'PAUSE' : 'PLAY' }
+                                    </button>
+
+                                    <button
+                                        style={{ margin: 0, verticalAlign: 'top' }}
+                                        onClick={ e => stopSound() }
+                                        className="light title">STOP</button>
+                                </div>
+                            </>
                     </div>
                 </div>
             </div>
             <div className="configBox fixedHeight">
-                {showFreq && <FrequencyDomainViz analyser={analyser} offset={playTime} />}
-                {showTime && <TimeDomainViz analyser={analyser} offset={playTime} />}
+                {showFreq && <FrequencyDomainViz playing={playing} rows={mappedRows} offset={playTime} />}
+                {showTime && <TimeDomainViz playing={playing} rows={mappedRows} offset={playTime} />}
             </div>
         </>
     )
